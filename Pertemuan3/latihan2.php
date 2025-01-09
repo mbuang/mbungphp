9c52af1e217215de7080cd02d0bd12e8fa620489
Zam <93270518+mbuang@users.noreply.github.com>
2025-01-09 15:02:12 +0700

    Struktur Kendali
    Pengulangan = for while do..while foreach : khusus array
    Percabangan = if, else, if else if else, ternary

---
+++ Pertemuan3/latihan2.php
@@ -0,0 +1,20 @@
+<?php 
+// Pengkondisian /Percabangan
+// if else
+// if else if else
+// ternary
+// switch
+
+
+$x = 20;
+if ($x < 20 ) {
+	echo "benar";
+}elseif ($x == 20) {
+	echo "binggo";
+} 
+
+else {
+	echo "salah";
+}
+
+ ?>
